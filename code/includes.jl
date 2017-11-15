6a946d61535831b6f6548f025b796dc82a108d3e
Matthieu Bulte <matthieu.bulte.06@gmail.com>
2017-11-15 09:22:10 +0100

    clean up proposal notebook by extracting some methods in other files

--- code/includes.jl
+++ code/includes.jl
@@ -1,6 +1,9 @@
 using PyPlot
 
+include("plotting.jl")
+
 @everywhere using Distributions
 @everywhere include("load_data.jl")
 @everywhere include("ode_solver.jl")
 @everywhere include("utils.jl")
+@everywhere include("montecarlos.jl")
