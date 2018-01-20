872824ce3603ee4d439c990b6073fd0cbcce6e4c
Matthieu Bulte <matthieu.bulte.06@gmail.com>
2018-01-20 10:32:04 +0100

    add 'exercise' for undirectly related work; normalize literature naming; add a plan for the bachelor thesis;

---
+++ plan/auto/references.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "kaipio2006statistical"
+    "hadamard"
+    "stuart_2010"
+    "liu2008monte"
+    "beskos_2015"
+    "chopin_2002"
+    "bayes-tutorial"))
+ :bibtex)
+
