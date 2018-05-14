547d9e02d9e64f257c0143d360d542426bcbadc8
Matthieu Bulte <matthieu.bulte.06@gmail.com>
2018-05-14 01:26:19 -0400

    first draft, the pain is real

--- code/includes.jl
+++ code/includes.jl
@@ -6,5 +6,5 @@ PyPlot.rc("figure", figsize=(10,8))
 include("plotting.jl")
 
 @everywhere using Distributions
+@everywhere using ODE
 @everywhere include("load_data.jl")
-@everywhere include("ode_solver.jl")
